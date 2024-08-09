fec8af6658cd52b0678111ecf2c5ca7bfa04f388
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-08-10 05:27:46 +0800

    add javascript for modal

--- js/script.js
+++ js/script.js
@@ -1,23 +1,15 @@
-// Get the modal
 const modal = document.getElementById("searchModal");
-
-// Get the button that opens the modal
 const modalButton = document.getElementById("openSearchModal");
+const closeButton = document.getElementById("closeModal");
 
-// Get the <span> element that closes the modal
-const closeButton = document.getElementsByClassName("modal__close-button");
-
-// When the user clicks the button, open the modal 
 modalButton.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-spcloseButton.onclick = function() {
+closeButton.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
