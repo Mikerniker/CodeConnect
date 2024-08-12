3f9dbd96bafda6b025054970ba870b712eaf399f
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-08-13 02:26:20 +0800

    remove search image and replace with modalButton for display onclick

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// MODAL 
+// MODAL VARIABLES
 
 const modal = document.getElementById("searchModal");
 const modalButton = document.getElementById("openSearchModal");
@@ -20,26 +20,26 @@ window.onclick = function(event) {
 }
 
 
-// Fries Menu
+// FRIES VARIABLES
 const logoImage = document.getElementById("ipsumLogo"); 
 const friesButton = document.getElementById("friesButton");
 const friesIcon = document.getElementById("friesIcon");
 const navigation = document.getElementById("navMenu");
 const enquiryButton = document.getElementById("enquiryButton"); 
 
-const searchImage = document.getElementById("searchIconImage"); 
 
-
-// Image Paths
+// FRIES IMAGE PATHS
 const friesMenuIconPath = "../images/components/fries-menu.svg";
 const closeIconPath = "../images/atoms/close.svg";
 const ipsumLogoPath = "../images/atoms/logoipsum.svg";
 const livertyLogoPath = "../images/blocks/livertylogo.svg";
 
+
+// FRIES MENU FUNCTIONS FUNCTIONS
 friesButton.onclick = function() {
   if (navigation.style.display === "flex") {
     navigation.style.display = "none";
-    searchImage.style.display = "flex";
+    modalButton.style.display = "flex";
 
     // Switch to fries icon
     friesIcon.src = friesMenuIconPath; 
@@ -51,12 +51,11 @@ friesButton.onclick = function() {
     
   } else {
     navigation.style.display = "flex";
-    searchImage.style.display = "none";
+    modalButton.style.display = "none";
 
     // Switch to X icon
     friesIcon.src = closeIconPath; 
     friesIcon.alt = "close menu icon"; 
-
 
     // Switch to Liverty logo
     logoImage.src = livertyLogoPath; 
