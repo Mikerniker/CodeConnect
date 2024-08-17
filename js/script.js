36abbf6085b86a0f68c1baaf367bcd1ffa54c3e1
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-08-18 05:08:26 +0800

    reorganize js variables

--- js/script.js
+++ js/script.js
@@ -4,22 +4,6 @@ const modal = document.getElementById("searchModal");
 const modalButton = document.getElementById("openSearchModal");
 const closeButton = document.getElementById("closeModal");
 
-// MODAL FUNCTIONS
-modalButton.onclick = function() {
-  modal.style.display = "block";
-}
-
-closeButton.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 // FRIES VARIABLES
 const logoImage = document.getElementById("ipsumLogo"); 
 const friesButton = document.getElementById("friesButton");
@@ -41,33 +25,79 @@ const ipsumLogoPath = "images/atoms/logoipsum.svg";
 const livertyLogoPath = "images/blocks/livertylogo.svg";
 
 
-
-// FRIES MENU FUNCTIONS FUNCTIONS
-friesButton.onclick = function() {
-  if (navigation.style.display === "flex") {
-    navigation.style.display = "none";
-    modalButton.style.display = "flex";
-
-    // Switch to fries icon
-    friesIcon.src = friesMenuIconPath; 
-    friesIcon.alt = "fries menu icon"; 
-
-    // Switch to Ipsum logo
-    logoImage.src = ipsumLogoPath; 
-    logoImage.alt = "ipsum logo"; 
-    
-  } else {
-    navigation.style.display = "flex";
-    modalButton.style.display = "none";
-
-    // Switch to X icon
-    friesIcon.src = closeIconPath; 
-    friesIcon.alt = "close menu icon"; 
-
-    // Switch to Liverty logo
-    logoImage.src = livertyLogoPath; 
-    logoImage.alt = "liverty logo"; 
-  }
+// MODAL FUNCTIONS
+modalButton.onclick = function() {
+  modal.style.display = "block";
 }
+
+
+closeButton.onclick = function() {
+  modal.style.display = "none";
+
+  // Revert fries menu state
+  if (navigation.style.display === "flex") {
+    navigation.style.display = "none";  // Hide navigation
+    modalButton.style.display = "flex"; // Show search button
+   
+}
+
+
+
+// closeButton.onclick = function() {
+//  modal.style.display = "none";
+
+ 
+//   // Revert the navigation to its original state
+//   navigation.style.display = "flex"; // Hide the navigation
+
+//    // Show the modal button
+//   //  modalButton.style.display = "flex"; 
+
+// //TO FIX
+  
+//   // if (modal.style.display === "none") {
+//   //   navigation.style.display = "flex";
+//   // } else {
+//   //   navigation.style.display = "flex";
+//   // }
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+
+
+
+// // FRIES MENU FUNCTIONS FUNCTIONS
+// friesButton.onclick = function() {
+//   if (navigation.style.display === "flex") {
+//     navigation.style.display = "none";
+//     modalButton.style.display = "flex";
+
+//     // Switch to fries icon
+//     friesIcon.src = friesMenuIconPath; 
+//     friesIcon.alt = "fries menu icon"; 
+
+//     // Switch to Ipsum logo
+//     logoImage.src = ipsumLogoPath; 
+//     logoImage.alt = "ipsum logo"; 
+    
+//   } else {
+//     navigation.style.display = "flex";
+//     modalButton.style.display = "none";
+
+//     // Switch to X icon
+//     friesIcon.src = closeIconPath; 
+//     friesIcon.alt = "close menu icon"; 
+
+//     // Switch to Liverty logo
+//     logoImage.src = livertyLogoPath; 
+//     logoImage.alt = "liverty logo"; 
+//   }
+// }
 
 
